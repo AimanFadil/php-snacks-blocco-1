208270a8a3c3e39e535417c4d2e38ba89912d843
AimanFadil <aimanbello2001@gmail.com>
2024-01-24 17:17:23 +0100

    snack 1

---
+++ snack1/snack1.php
@@ -0,0 +1,49 @@
+<?php
+    $squadre = [
+        [
+            'squadraCasa' => 'Brooklin Nets',
+            'squadraOspite' => 'New York Knicks',
+            'puntiCasa' => 100,
+            'puntiOspite' => 98,
+
+        ],
+        [
+            'squadraCasa' => 'Los Angeles Lakers',
+            'squadraOspite' => 'Los Angeles Clippers',
+            'puntiCasa' => 83,
+            'puntiOspite' => 91,
+
+        ],
+        [
+            'squadraCasa' => 'Golden State',
+            'squadraOspite' => 'Memphis Griezles',
+            'puntiCasa' => 115,
+            'puntiOspite' => 75,
+
+        ],
+        [
+            'squadraCasa' => 'Pheonix Suns',
+            'squadraOspite' => 'Boston',
+            'puntiCasa' => 91,
+            'puntiOspite' => 70,
+
+        ],
+        [
+            'squadraCasa' => 'Indiana Pacers',
+            'squadraOspite' => 'Orlando Magic',
+            'puntiCasa' => 121,
+            'puntiOspite' => 88,
+
+        ]
+    ]
+    
+?>
+<?php for($i = 0; $i < count($squadre); $i++){ ?>
+    
+    <p>
+        <?php foreach($squadre[$i] as $squadra){
+            echo $squadra.'   ';
+        } ?>
+    </p>
+<?php }?>
+
